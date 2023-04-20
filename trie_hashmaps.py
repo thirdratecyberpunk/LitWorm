896fb33849d066169b10f719600d5e8b4eec4f56
thirdratecyberpunk <blackbul@aston.ac.uk>
2023-04-20 17:51:25 +0100

    Removed unneeded methods and create trie class definition

--- trie_hashmaps.py
+++ trie_hashmaps.py
@@ -8,91 +8,85 @@ import string
 # defining our alphabet (probably can modify this to work for non-English letters)
 ALPHABET = list(string.ascii_lowercase)
 SIZE = len(ALPHABET)
- 
+
+class Trie:
+    def __init__(self, filename="assets/words_alpha.txt"):
+        print("Loading dictionary...")
+        self.dict = loadtxt(filename, dtype=str,comments="#", delimiter=",", unpack=False)
+        print("Dictionary loaded!")
+        print("Inserting data")
+        self.root = TrieNode()
+        
+        # insert all words of dictionary into trie
+        for word in self.dict:
+            self.insert(word)
+        print("Data inserted!")
+    
+    # If not present, inserts key into trie
+    # If the key is prefix of trie node, just
+    # marks leaf node
+    def insert(self, key):
+        p_child = self.root
+        for i, char in enumerate(key):
+            if char not in p_child.children:
+                prefix = key[0:i+1]
+                p_child.children[char] = TrieNode(prefix)
+            p_child = p_child.children[char]
+        # make last node as leaf node
+        p_child.leaf = True
+
+    def get_all_words_from_set_of_letters(self, root, letter_set, words=set()):
+        """
+        Finds all words that can be constructed from a given
+        set of letters
+        """
+        # set the current node to explore to the root given 
+        # from the first call
+        # let's say you have C,A,T
+        # C is the root node
+        # then, need to check CA
+        # then, need to check CT
+        # then, make A the root node
+        # then, check AC
+        # then, check AT
+        # then, need to make T the root node
+        # then, need to check TC
+        # then, need to check TA
+        # finally, need to return the set of words that are valid
+        # in this case, it should be C, A, T, TA, AT, CAT and TAC
+        current = root
+        root.visited = True
+        for letter in letter_set:
+            other_letters = letter_set.copy()
+            other_letters.remove(letter)
+            if letter in root.children: 
+                current = root.children[letter]
+                if current.leaf and current.text not in words:
+                    words.add(current.text)
+                # if we haven't already explored this node
+                if current.visited is False:
+                    for other_letter in other_letters:
+                        self.get_all_words_from_set_of_letters(current, other_letters, words)
+                else:
+                    pass
+        return words
+
 # trie Node
 class TrieNode:
-    def __init__(self):
+    def __init__(self, text=''):
+        # string representing text of node
+        self.text = text
         # hashmap of child nodes mapping letter (key) to node (value)
         self.children = {}
         # does node represents end of a word?
         self.leaf = False
- 
-# Returns new trie node (initialized to NULLs)
-def get_node():
-    new_node = TrieNode()
-    return new_node
-
-# If not present, inserts key into trie
-# If the key is prefix of trie node, just
-# marks leaf node
-def insert_hashmap(root, key):
-    n = len(key)
-    p_child = root
-    for char in key:
-        if char not in p_child.children:
-            p_child.children[char] = get_node()
-        p_child = p_child.children[char]
-    # make last node as leaf node
-    p_child.leaf = True
- 
-# A recursive function to print all possible valid
-# words present in array
-def searchWord(root, hash, string):
-    # if this node is the leaf node
-    # we have found a valid word
-    if root.leaf:
-        print(string)
-    # otherwise, recursively explore all child nodes
-    # of current node
-
-    # for each letter in the alphabet
-    for letter in ALPHABET:
-        # if this letter IS in our set of letters to explore
-        # and there is a child node from this node
-        if letter in hash and letter in root.children:
-            # recursively search the trie for if there is a sequence with
-            # the string + concatenated node
-            searchWord(root.children[letter], hash, string + letter)
- 
-# Prints all words present in dictionary.
-def print_all_words(arr, root, n):
-    # creates an array storing if each letter in 
-    # the alphabet is in the characters to explore
-    hash = arr
-         
-    # temporary node
-    p_child = root
-     
-    # string to hold output words
-    string = ""
-     
-    # Traverse all matrix elements. There are only 26
-    # character possible in char array
-    for letter in ALPHABET:
-        # we start searching for word in dictionary
-        # if we found a character which is child
-        # of Trie root
-        if letter in hash and letter in root.children:
-            string = string + letter
-            searchWord(p_child.children[letter], hash, string)
-            string = ""
+        # has this node already BEEN the root node?
+        self.visited = False
  
 # Driver program to test above function
 if __name__ == '__main__':
-    # Let the given dictionary be following
-    print("Loading dictionary...")
-    dict = loadtxt("assets/words_alpha.txt", dtype=str,comments="#", delimiter=",", unpack=False)
-    print("Dictionary loaded!")
-    print("Inserting data")
-    root = get_node()
-     
-    # insert all words of dictionary into trie
-    n = len(dict)
-    for word in dict:
-        insert_hashmap(root, word)
-    
-    print("Data inserted!")
-    arr = ['a', 'c', 't', 'o']
-    n = len(arr)
+    trie = Trie()
+    # arr = ['y', 'u','o','s','r','p','t','u','a','y','q','b','o','l','p','d']
+    arr = ['c', 'a', 't', 'a','o']
     print(f"Checking {arr}")
-    print_all_words(arr, root, n)
+    print(trie.get_all_words_from_set_of_letters(root=trie.root, letter_set=arr))
