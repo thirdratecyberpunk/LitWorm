dee4ba39c13278d63416f7cd9b652cebb3452965
thirdratecyberpunk <blackbul@aston.ac.uk>
2023-04-20 19:17:03 +0100

    Removed words consisting of only a single letter repeated

--- dict_sanitisation.py
+++ dict_sanitisation.py
@@ -5,8 +5,11 @@ import string
 def is_valid_word(word):
     """
     Returns if a word meets the given criteria for it to be included in the dictionary
+    Current criteria:
+    - word is >2 characters long (i.e. no NO)
+    - word has at least 2 unique letters (i.e. no AAA)
     """
-    return len(line) > 2
+    return len(line) > 2 and len(set(word)) > 1
 
 lines = loadtxt("assets/words_alpha.txt", dtype=str,comments="#", delimiter=",", unpack=False)
 
